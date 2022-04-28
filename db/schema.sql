69d5d98e8bc796bf78aa57d890e27cbac32320a5
laianribas <laian67a@gmail.com>
2022-04-27 21:07:03 -0300

    criando base de dadeos

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE csi_database;
+
+CREATE TABLE csi_database.accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+  last_login TIMESTAMP 
+);
